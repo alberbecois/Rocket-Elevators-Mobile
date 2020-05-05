f124bc0d5fbc8712ee8af9ec3605c439717b1908
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-05-05 16:03:55 -0400

    Initial Commit

---
+++ Rocket Elevators Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rocket Elevators Mobile
+//
+//  Created by Joshua Knutson on 2020-05-05.
+//  Copyright © 2020 Joshua Knutson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
